4550e6c3ef8580653c087c16d9ffea935d61ecf3
Jinzhao <jil016@eng.ucsd.edu>
2020-10-20 00:21:32 -0700

    rotate image, add intrinsic params. header?

---
+++ main.py
@@ -0,0 +1,93 @@
+import rosbag
+import yaml
+import cv2
+from cv_bridge import CvBridge
+
+'''
+Write
+    def write(self, topic, msg, t=None, raw=False, connection_header=None):
+        """
+        Write a message to the bag.
+        @param topic: name of topic
+        @type  topic: str
+        @param msg: message to add to bag, or tuple (if raw)
+        @type  msg: Message or tuple of raw message data
+        @param t: ROS time of message publication, if None specifed, use current time [optional]
+        @type  t: U{genpy.Time}
+        @param raw: if True, msg is in raw format, i.e. (msg_type, serialized_bytes, md5sum, pytype)
+        @type  raw: bool
+        @raise ValueError: if arguments are invalid or bag is closed
+        """
+'''
+
+def readBagInfo(bag):
+    return yaml.load(bag._get_yaml_info())
+
+def readBagTopicList(bag):
+    topicList = []
+    for topic, _, _ in bag.read_messages():
+        if topicList.count(topic) == 0:
+            topicList.append (topic)
+
+    print '{0} topics found:'.format(len(topicList))
+    return topicList
+
+def addImageTopic():
+    pass
+
+def removeImageTopic():
+    pass
+
+def modifyImageTopic(bag, topic):
+    pass
+
+def modifyStdMsgTopic(bag, topic):
+    pass
+
+if __name__ == '__main__':
+    '''
+    /clock
+    /flea3/camera_info
+    /flea3/image_raw
+    /mavros/distance_sensor/lidarlite_pub
+    /mavros/imu/atm_pressure
+    /mavros/imu/data
+    /mavros/imu/data_raw
+    /mavros/imu/mag
+    /mavros/local_position/pose
+    /mavros/local_position/velocity
+    /mavros/rc/in
+    /mavros/state
+    /rosout
+    /rosout_agg
+    '''
+
+    bag = rosbag.Bag('/home/erl/rosbag/mit_medfield.bag', 'r')
+    topic_list = readBagTopicList(bag)
+
+
+    with rosbag.Bag('/home/erl/rosbag/mit_medfield_modified.bag', 'w') as outbag:
+        for idx, topic_name in enumerate(topic_list):
+            for topic, msg, t in bag.read_messages(topics=[topic_name]):
+            # This also replaces tf timestamps under the assumption 
+            # that all transforms in the message share the same timestamp
+                if topic == "/flea3/camera_info": # add K, D
+                    msg.K = (484.0, 0.0, 320.0, 0.0, 484.0, 262.5, 0.0, 0.0, 1.0)
+                    msg.D = (0.0, 0.0, 0.0, 0.0, 0.0)
+                    outbag.write(topic, msg, t)
+                elif topic == "/flea3/image_raw": # rotate 180 deg
+                    bridge = CvBridge()
+                    
+                    orig_img = bridge.imgmsg_to_cv2(msg, desired_encoding='mono8')
+                    rot_img = cv2.rotate(orig_img, cv2.ROTATE_180)
+                    rot_msg = bridge.cv2_to_imgmsg(rot_img, encoding="mono8")
+                    rot_msg.header = msg.header
+                    outbag.write(topic, rot_msg, t)
+                else:
+                    outbag.write(topic, msg, t)
+            
+            print("Finished processing topic: {}; Progress: {}/{}".format(topic_name, idx, len(topic_list)))
+            
+    print("DONE!")
+    
+    
