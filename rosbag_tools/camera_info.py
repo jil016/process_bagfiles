48a4a03fed12f033f5af5eb970ecc3d0a3eae9fe
Jinzhao <jil016@eng.ucsd.edu>
2020-11-30 17:02:10 -0800

    adding camera_info

--- rosbag_tools/camera_info.py
+++ rosbag_tools/camera_info.py
@@ -116,6 +116,90 @@ def testModifiedBag(mdf_path="/home/erl/rosbag/mit_medfield_modified.bag"):
     return
 
 
+def modifyMIT_orcvio(
+    inpath="/home/erl/rosbag/indoor_vio5_2020-11-03-18-44-15.bag",
+    outpath="/home/erl/rosbag/indoor_vio5_2020-11-03-18-44-15_modified.bag",
+):
+    """
+    Topic list:
+        acl_jackal/forward/imu                                                11608 msgs    : sensor_msgs/Imu                           
+        acl_jackal/forward/infra1/camera_info                                  1742 msgs    : sensor_msgs/CameraInfo                    
+        acl_jackal/forward/infra1/image_rect_raw/compressed                    1742 msgs    : sensor_msgs/CompressedImage               
+        acl_jackal/forward/infra2/camera_info                                  1742 msgs    : sensor_msgs/CameraInfo                    
+        acl_jackal/forward/infra2/image_rect_raw/compressed                    1742 msgs    : sensor_msgs/CompressedImage  
+    """
+    bag = rosbag.Bag(inpath, "r")
+    topic_list = readBagTopicList(bag)
+
+    with rosbag.Bag(outpath, "w") as outbag:
+        for idx, topic_name in enumerate(topic_list):
+            for topic, msg, t in bag.read_messages(topics=[topic_name]):
+                # This also replaces tf timestamps under the assumption
+                # that all transforms in the message share the same timestamp
+                if (
+                    topic
+                    == "acl_jackal/forward/infra1/image_rect_raw/compressed"
+                    or topic
+                    == "acl_jackal/forward/infra2/image_rect_raw/compressed"
+                ):
+                    bridge = CvBridge()
+                    img = bridge.compressed_imgmsg_to_cv2(msg)
+                    mod_msg = bridge.cv2_to_imgmsg(img, encoding="mono8")
+                    mod_msg.header = msg.header
+                    outbag.write(topic, mod_msg, t)
+                else:
+                    outbag.write(topic, msg, t)
+            print(
+                "Finished processing topic: {}; Progress: {}/{}".format(
+                    topic_name, idx, len(topic_list)
+                )
+            )
+
+    print("DONE!")
+    return
+
+
+def buildCameraInfo():
+    from sensor_msgs.msg import CameraInfo
+
+    camera_info = CameraInfo()
+    # store info without header
+    camera_info.width = int(640)
+    camera_info.height = int(400)
+    camera_info.distortion_model = "plumb_bob"
+    fx = 245.96
+    fy = 246.03
+    cx = 313.42
+    cy = 195.63
+    camera_info.K = [fx, 0, cx, 0, fy, cy, 0, 0, 1]
+    camera_info.D = [0, 0, 0, 0, 0]
+    camera_info.R = [1.0, 0, 0, 0, 1.0, 0, 0, 0, 1.0]
+    camera_info.P = [fx, 0, cx, 0, 0, fy, cy, 0, 0, 0, 1.0, 0]
+    return camera_info
+
+
+def addCameraInfo(
+    inpath="/home/erl/rosbag/indemind_mesa_test_2.bag",
+    outpath="/home/erl/rosbag/indemind_mesa_test_2_added_left_info.bag",
+):
+
+    bag = rosbag.Bag(inpath, "r")
+    img_topic_name = "/indemind/left/image/compressed"
+    caminfo_topic = "/indemind/left/camera_info"
+    camera_info = buildCameraInfo()
+    topic_list = readBagTopicList(bag)
+
+    with rosbag.Bag(outpath, "w") as outbag:
+        for idx, topic_name in enumerate(topic_list):
+            for topic, msg, t in bag.read_messages(topics=[topic_name]):
+                if topic == img_topic_name:
+                    outbag.write(caminfo_topic, camera_info, t)
+                outbag.write(topic, msg, t)
+    return
+
+
 if __name__ == "__main__":
-    modifyMITMedfield()
-    testModifiedBag()
+    # modifyMITMedfield()
+    # testModifiedBag()
+    # modifyMIT_orcvio()
+    addCameraInfo()
